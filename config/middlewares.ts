4289226c1013f33fd8380f8d073a5b8c124fd252
Jesse Smith <jesseholden713@gmail.com>
2025-12-02 19:45:29 -0800

    fixed cors config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,16 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   {
-    name: "strapi::cors",
+    name: 'strapi::cors',
     config: {
+      enabled: true,
       origin: [
-        "http://localhost:3000",
-        "https://portfolio-site-redesign-qwc5qtu4y-jesses-projects-98ea59e3.vercel.app/",
-        "https://www.jessesmith.tech",
-        "https://jessesmith.tech"
+        'http://localhost:3000',
+        'https://portfolio-site-redesign.vercel.app',
+        'https://portfolio-site-redesign-qwc5qtu4y-jesses-projects-98ea59e3.vercel.app',
+        'https://www.jessesmith.tech',
+        'https://jessesmith.tech'
       ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-      headers: "*",
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeadersOnError: true,
     },
   },
   'strapi::security',
